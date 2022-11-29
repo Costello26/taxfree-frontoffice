ca4722e43bfcf54fb6ad8f0232398386d34ab160
Khurmatillo <khurmatillomamasoliev@gmail.com>
2022-11-29 10:10:59 +0500

    feat: registration view created

---
+++ src/layout/Registration/Registration.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from '../../components/AppBar/AppBar';
+import ScanCheck from '../../components/ScanCheck/ScanCheck';
+
+const Registration = () => {
+  return (
+    <div className="container">
+      <AppBar />
+      <ScanCheck />
+    </div>
+  );
+};
+
+export default Registration;
