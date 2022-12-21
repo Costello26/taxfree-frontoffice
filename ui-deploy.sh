c0dc0269b4067f3d1af657b79ffe9ca697736ac8
Muxammadkarim Fayziyev <muxammadkarim.fayziyev@soliq.uz>
2022-12-21 13:19:08 +0000

    Update ui-deploy.sh

--- ui-deploy.sh
+++ ui-deploy.sh
@@ -6,4 +6,4 @@ eval $(ssh-agent -s)
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 ssh-add <(echo "$1")
 
-scp -oStrictHostKeyChecking=no -r build/* taxfree@172.24.56.1:/home/taxfree/www
+scp -oStrictHostKeyChecking=no -r build/* taxfree@172.24.56.1:/home/taxfreeoffice/www
